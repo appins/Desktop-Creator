20095886c22d5e3bbab009c10c6af1335a432556
Alex Anderson <alexandersonone@gmail.com>
2015-09-27 12:35:18 -0700

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-say "This is a test";
 
-say "It worked!";
+
+
+say "ROCK PAPER SCISSORS!";
+echo "ROCK PAPER SCISSORS.";
+while true;
+do
+dynAi= 0;
+read usrCh;
+usrChN= 0;
+if [ usrCh == "rock" ]; then
+  usrChN= 0;
+elif [ usrCh == "paper"]; then
+  usrChN= 1;
+elif [ usrCh == "scissors"]; then 
+  usrChN= 2;
+fi
+done
