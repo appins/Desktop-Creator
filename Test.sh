bb5f47c4f963ef2d5557939247889d0b528f3976
Alex Anderson <alexandersonone@gmail.com>
2015-09-27 18:07:51 -0700

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
+echo "Pick a directroy at the location of ~/../..";
+read dirName;
+cd "~/../../$dirname";
 
-
-
-say "ROCK PAPER SCISSORS!";
-echo "ROCK PAPER SCISSORS.";
-while true;
-do
-dynAi= 0;
-read usrCh;
-usrChN= 0;
-if [ usrCh = "rock" ]; then
-  usrChN= 0;
-elif [ usrCh = "paper"]; then
-  usrChN= 1;
-elif [ usrCh = "scissors"]; then 
-  usrChN= 2;
-fi
-done
