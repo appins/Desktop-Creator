5e310d24b15f9ca1228e94bbe78f04bdf1f28ec2
Alex Anderson <alexandersonone@gmail.com>
2015-10-01 15:32:05 +0000

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,8 +1,14 @@
-echo "This directory will be used.";
+echo "This directory will be used."; pwd;
+echo "Press ctrl-c to cancel...";
+sleep 3;
 echo "How many copys?";
 read cpn;
 echo "What file?";
 read fn;
-
-echo "$fn $cpn";
+a=0
+while [ $a < cpn ]
+do
+a=$((a+1));
+cp "$fn" "$a .jpg";
+done
 
