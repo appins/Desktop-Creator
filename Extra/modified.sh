df651ac4c350e12b944d87109186eddec3eccf30
AppIns <AlexAndersonOne@gmail.com>
2015-10-02 16:58:01 -0700

    Test2

--- Extra/modified.sh
+++ Extra/modified.sh
@@ -3,12 +3,3 @@
 echo "This progrm is modified, the original can be found at ";
 echo "GitHub.com/AppIns/Test";
 echo "Do you wish to continue?";
-
-
-
-read ans;
-if [ ans=="yes" ]; then
-  :
-else
-  exit;
-fi
