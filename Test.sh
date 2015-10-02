feb68a13961a800c6c8bb6c40ccd06823e044bd2
AppIns <AlexAndersonOne@gmail.com>
2015-10-01 22:38:05 -0700

    test

--- Test.sh
+++ Test.sh
@@ -1,14 +1,1 @@
-echo "This directory will be used."; pwd;
-echo "Press ctrl-c to cancel...";
-sleep 3;
-echo "How many copys?";
-read cpn;
-echo "What file?";
-read fn;
-a=0
-while [ $a < cpn ]
-do
-a=$((a+1));
-cp "$fn" "$a .jpg";
-done
-
+say "test";
