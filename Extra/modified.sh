7d524609c2656fcdd7d5eaa9e5847a6fd090769d
AppIns <AlexAndersonOne@gmail.com>
2015-10-02 16:44:19 -0700

    Update v0.0.1

---
+++ Extra/modified.sh
@@ -0,0 +1,14 @@
+# Use this if the program was modified
+
+echo "This progrm is modified, the original can be found at ";
+echo "GitHub.com/AppIns/Test";
+echo "Do you wish to continue?";
+
+
+
+read ans;
+if [ ans=="yes" ] then
+  
+else
+  exit;
+fi
--- Test.sh
+++ Test.sh
@@ -1,1 +1,4 @@
-say "test";
+# This program is licensed under the MIT Public Licsense v1.0
+
+# Uncomment line bellow if this is modified
+#bash <(curl -s Null)
