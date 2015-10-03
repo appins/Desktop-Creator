80a576ddbea368d43680cb8c3327d0cc4bab24f4
AppIns <AlexAndersonOne@gmail.com>
2015-10-02 17:17:49 -0700

    v0.0.2

--- Test.sh
+++ Test.sh
@@ -1,6 +1,16 @@
 # This program is licensed under the MIT Public Licsense v1.0
 
 # Uncomment line bellow if this is modified
-bash <(curl -s https://raw.githubusercontent.com/AppIns/Test/master/Extra/modified.sh)
+#bash <(curl -s https://raw.githubusercontent.com/AppIns/Test/master/Extra/modified.sh)
 
-say "test";
+echo -n "Supply a file (~/): ";
+read filename;
+echo "is $filename correct: ";
+read option;
+if [ "$option"=="yes" ]; then
+  cd ~/Desktop/
+  echo -e "echo \"Starting $filename\"bash ~/$filename" > "$filename.sh";
+  chmod 777 "$filename.sh";
+else
+  echo "Please re-run this file";
+fi
