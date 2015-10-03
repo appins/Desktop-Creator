8141bd17a26889da7ce7cabc57523b4677b1f0bb
AppIns <AlexAndersonOne@gmail.com>
2015-10-02 18:09:31 -0700

    v0.0.2

--- Test.sh
+++ Test.sh
@@ -1,4 +1,4 @@
-# This program is licensed under the MIT Public Licsense v1.0
+# This program is licensed under the Eclipse Public Licsense v1.0
 
 # Uncomment line bellow if this is modified
 #bash <(curl -s https://raw.githubusercontent.com/AppIns/Test/master/Extra/modified.sh)
