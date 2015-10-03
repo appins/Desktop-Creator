38f245b1004abe29664af7143819d0474274c081
AppIns <AlexAndersonOne@gmail.com>
2015-10-02 18:08:29 -0700

    v0.0.2

--- Test.sh
+++ Test.sh
@@ -3,14 +3,17 @@
 # Uncomment line bellow if this is modified
 #bash <(curl -s https://raw.githubusercontent.com/AppIns/Test/master/Extra/modified.sh)
 
-echo -n "Supply a file (~/): ";
+echo -n "Supply a file (~): ";
 read filename;
 echo "is $filename correct: ";
 read option;
 if [ "$option"=="yes" ]; then
   cd ~/Desktop/
-  echo -e "echo \"Starting $filename\"bash ~/$filename" > "$filename.sh";
-  chmod 777 "$filename.sh";
+  echo -e "[Desktop Entry]
+Type=Application
+Exec=$HOME/$filename
+Terminal=true" > "Start.desktop";
+chmod +x Start.desktop;
 else
   echo "Please re-run this file";
 fi
